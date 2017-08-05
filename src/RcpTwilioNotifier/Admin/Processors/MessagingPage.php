cc226e2c3ed14cbb498795e2a71c3a9ad555a614
Lucas Cherkewski <lucas@ecustom.ca>
2017-08-05 14:16:42 -0700

    initial processing functions

--- src/RcpTwilioNotifier/Admin/Processors/MessagingPage.php
+++ src/RcpTwilioNotifier/Admin/Processors/MessagingPage.php
@@ -7,6 +7,8 @@
  */
 
 namespace RcpTwilioNotifier\Admin\Processors;
+use RcpTwilioNotifier\Models\Member;
+use RcpTwilioNotifier\Models\Region;
 
 /**
  * Processes form submissions from our MessagingPage in the WordPress admin.
@@ -31,7 +33,37 @@ class MessagingPage extends AbstractProcessor implements ProcessorInterface {
 	 * Process!
 	 */
 	public function process() {
-		echo 'processing!'; // @TODO: Implement processor
+		$this->region = new Region( $_POST['rcptn_region'] ); // WPCS: CSRF ok.
+		$this->message = $_POST['rcptn_message']; // WPCS: CSRF ok.
+	}
+
+	/**
+	 * Message all the members within a region.
+	 *
+	 * @param Region $region  The region whose members we'll message.
+	 */
+	public function message_all_in_region( Region $region ) {
+		// @TODO: pull in members from other regions, who subscrbie to all regions
+		$members = $region->get_members();
+
+		foreach ( $members as $member ) {
+			$this->message_member( $member );
+		}
+	}
+
+	/**
+	 * Message a given member, checking first that they’re eligible to receive messages.
+	 *
+	 * @param Member $member  The member to message.
+	 *
+	 * @return \Twilio\Rest\Api\V2010\Account\MessageInstance|void
+	 */
+	public function message_member( Member $member ) {
+		if ( ! $member->is_active() ) {
+			return;
+		}
+
+		return $member->send_message( $this->message );
 	}
 
 }
