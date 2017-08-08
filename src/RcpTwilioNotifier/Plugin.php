30701d165ba8341e48f62e7a64f0c24452418c2e
Lucas Cherkewski <lucas@ecustom.ca>
2017-08-07 17:38:44 -0700

    add comments

--- src/RcpTwilioNotifier/Plugin.php
+++ src/RcpTwilioNotifier/Plugin.php
@@ -51,9 +51,21 @@ class Plugin {
 	 * Let's go!
 	 */
 	public function load() {
+		/**
+		 * Settings verification
+		 */
 		$settings_verifier = new Admin\SettingsVerification();
 		$settings_verifier->init();
 		$settings_verifier->remind_if_settings_not_present();
+
+		/**
+		 * Admin pages and their processors
+		 */
+		$admin_messaging_page = new Admin\Pages\MessagingPage( $this->regions );
+		$admin_messaging_page->init();
+
+		$admin_messaging_page_processor = new Admin\Pages\Processors\MessagingPage( $this->regions );
+		$admin_messaging_page_processor->init();
 
 		$admin_settings_page = new Admin\Pages\SettingsPage();
 		$admin_settings_page->init();
@@ -61,6 +73,9 @@ class Plugin {
 		$admin_settings_page_processor = new Admin\Pages\Processors\SettingsPage();
 		$admin_settings_page_processor->init();
 
+		/**
+		 * Member fields
+		 */
 		$region_registration_field = new Admin\MemberFields\Region\Registration( $this->regions );
 		$region_registration_field->init();
 
@@ -72,12 +87,6 @@ class Plugin {
 
 		$phone_number_edit_member_field = new Admin\MemberFields\PhoneNumber\EditMember();
 		$phone_number_edit_member_field->init();
-
-		$admin_messaging_page = new Admin\Pages\MessagingPage( $this->regions );
-		$admin_messaging_page->init();
-
-		$admin_messaging_page_processor = new Admin\Pages\Processors\MessagingPage( $this->regions );
-		$admin_messaging_page_processor->init();
 	}
 
 }
