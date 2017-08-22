3b6029f24116263597ccac9f291bcd16642a0bab
Lucas Cherkewski <lucas@ecustom.ca>
2017-08-22 18:46:06 -0400

    convert registration handler to a class

--- assets/js/registration_handler.js
+++ assets/js/registration_handler.js
@@ -5,52 +5,79 @@
  * @subpackage RcpTwilioNotifier
  */
 
-(function($) {
-	var rcp_registration_form   = document.getElementById( 'rcp_registration_form' ),
-		rcp_subscription_levels = document.getElementById( 'rcp_subscription_levels' );
+/**
+ * RCPTN Registration Controller
+ *
+ * Manages RCPTN modifications to RCP registration form.
+ */
+var RCPTN_Registration_Controller = {
+	$rcp_subscription_levels_list: jQuery( '#rcp_subscription_levels' ),
 
-	if ( null === rcp_registration_form ) {
-		return;
-	}
+	addonInputTemplate: '<div>' +
+							'<input type="checkbox" id="rcptn_all_regions_addon">' +
+							'<label for="rcptn_all_regions_addon">Enable all regions add-on</label>' +
+						'</div>',
 
-	if ( null === rcp_subscription_levels ) {
-		return;
-	}
-
-	$( '#rcp_subscription_levels' ).after( rcptn_get_addon_template() );
-
-	rcptn_toggle_subscription_inputs( 'basic' );
-
-	$( '#rcptn_all_regions_addon' ).on(
-		'click', function(e) {
-			if ( $( this )[0].checked ) {
-				rcptn_toggle_subscription_inputs( 'addon' );
-			} else {
-				rcptn_toggle_subscription_inputs( 'basic' );
-			}
+	/**
+	 * Class setup
+	 */
+	init: function() {
+		if ( null === document.getElementById( 'rcp_registration_form' ) ) {
+			return;
 		}
-	);
-})( jQuery );
 
-function rcptn_get_addon_template() {
-	return '<div>' +
-		'<input type="checkbox" id="rcptn_all_regions_addon">' +
-		'<label for="rcptn_all_regions_addon">Enable all regions add-on</label>' +
-		'</div>';
-}
-
-function rcptn_toggle_subscription_inputs( enabled_level ) {
-	var $rcp_subscription_levels_list = jQuery( '#rcp_subscription_levels' );
-
-	jQuery.each(
-		rcptn_registration_handler_data.associated_subscription_ids, function( index, subscription_pair ) {
-			if ( 'basic' === enabled_level ) {
-				$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.basic_level ).show();
-				$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.addon_level ).hide();
-			} else if ( 'addon' === enabled_level ) {
-				$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.addon_level ).show();
-				$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.basic_level ).hide();
-			}
+		if ( null === document.getElementById( 'rcp_subscription_levels' ) ) {
+			return;
 		}
-	);
-}
+
+		this.insertAddonInput();
+		this.toggleSubscriptionInputs( 'basic' );
+		this.setListeners();
+	},
+
+	insertAddonInput: function() {
+		jQuery( '#rcp_subscription_levels' ).after( this.addonInputTemplate );
+	},
+
+	toggleSubscriptionInputs: function( enabled_level ) {
+		var that = this;
+
+		jQuery.each(
+			rcptn_registration_handler_data.associated_subscription_ids, function( index, subscription_pair ) {
+				if ( 'basic' === enabled_level ) {
+					that.$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.basic_level ).show();
+					that.$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.addon_level ).hide();
+				} else if ( 'addon' === enabled_level ) {
+					that.$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.addon_level ).show();
+					that.$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.basic_level ).hide();
+				}
+			}
+		);
+	},
+
+	/**
+	 * Listeners to respond to various actions
+	 */
+	setListeners: function() {
+		var that = this;
+
+		jQuery( '#rcptn_all_regions_addon' ).on(
+			'click', function(e) {
+				if ( jQuery( this )[0].checked ) {
+					that.toggleSubscriptionInputs( 'addon' );
+				} else {
+					that.toggleSubscriptionInputs( 'basic' );
+				}
+			}
+		);
+	}
+};
+
+// Make the registration class globally available.
+window['RCPTN_Registration_Controller'] = RCPTN_Registration_Controller;
+
+jQuery( document ).ready(
+	function() {
+		RCPTN_Registration_Controller.init();
+	}
+);
