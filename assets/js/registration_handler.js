53361ed56ffddf144bfb1483fac0046c58ed7a79
Lucas Cherkewski <lucas@ecustom.ca>
2017-08-22 18:32:12 -0400

    toggle levels according to addon

--- assets/js/registration_handler.js
+++ assets/js/registration_handler.js
@@ -17,13 +17,17 @@
 		return;
 	}
 
-	var $rcp_subscription_levels_list = $( '#rcp_subscription_levels' );
+	$( '#rcp_subscription_levels' ).after( rcptn_get_addon_template() );
 
-	$rcp_subscription_levels_list.after( rcptn_get_addon_template() );
+	rcptn_toggle_subscription_inputs( 'basic' );
 
-	$.each(
-		rcptn_registration_handler_data.associated_subscription_ids, function( index, subscription_pair ) {
-			$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.addon_level ).hide();
+	$( '#rcptn_all_regions_addon' ).on(
+		'click', function(e) {
+			if ( $( this )[0].checked ) {
+				rcptn_toggle_subscription_inputs( 'addon' );
+			} else {
+				rcptn_toggle_subscription_inputs( 'basic' );
+			}
 		}
 	);
 })( jQuery );
@@ -33,4 +37,20 @@ function rcptn_get_addon_template() {
 		'<input type="checkbox" id="rcptn_all_regions_addon">' +
 		'<label for="rcptn_all_regions_addon">Enable all regions add-on</label>' +
 		'</div>';
+}
+
+function rcptn_toggle_subscription_inputs( enabled_level ) {
+	var $rcp_subscription_levels_list = jQuery( '#rcp_subscription_levels' );
+
+	jQuery.each(
+		rcptn_registration_handler_data.associated_subscription_ids, function( index, subscription_pair ) {
+			if ( 'basic' === enabled_level ) {
+				$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.basic_level ).show();
+				$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.addon_level ).hide();
+			} else if ( 'addon' === enabled_level ) {
+				$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.addon_level ).show();
+				$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.basic_level ).hide();
+			}
+		}
+	);
 }
