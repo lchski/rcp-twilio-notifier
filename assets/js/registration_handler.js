48c6caeac2f5c7e19fe397d5683c2886a8f4b24a
Lucas Cherkewski <lucas@ecustom.ca>
2017-08-22 18:25:29 -0400

    add checkbox for addon

--- assets/js/registration_handler.js
+++ assets/js/registration_handler.js
@@ -19,9 +19,18 @@
 
 	var $rcp_subscription_levels_list = $( '#rcp_subscription_levels' );
 
+	$rcp_subscription_levels_list.after( rcptn_get_addon_template() );
+
 	$.each(
 		rcptn_registration_handler_data.associated_subscription_ids, function( index, subscription_pair ) {
 			$rcp_subscription_levels_list.find( '.rcp_subscription_level_' + subscription_pair.addon_level ).hide();
 		}
 	);
 })( jQuery );
+
+function rcptn_get_addon_template() {
+	return '<div>' +
+		'<input type="checkbox" id="rcptn_all_regions_addon">' +
+		'<label for="rcptn_all_regions_addon">Enable all regions add-on</label>' +
+		'</div>';
+}
