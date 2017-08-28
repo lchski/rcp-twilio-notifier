de698e3e429eca1aa7631bea4d5d8d41b7a75144
Lucas Cherkewski <lucas@ecustom.ca>
2017-08-28 12:15:41 -0400

    notices with IDs now

--- src/RcpTwilioNotifier/Models/Notice.php
+++ src/RcpTwilioNotifier/Models/Notice.php
@@ -14,6 +14,13 @@ namespace RcpTwilioNotifier\Models;
 class Notice {
 
 	/**
+	 * A unique ID for the notice.
+	 *
+	 * @var string
+	 */
+	public $ID;
+
+	/**
 	 * The type of the notice.
 	 *
 	 * @var 'error'|'warning'|'success'|'info'
@@ -30,12 +37,27 @@ class Notice {
 	/**
 	 * Set internal values.
 	 *
-	 * @param string $type  The notice's type (one of 'error'|'warning'|'success'|'info').
-	 * @param mixed  $message  The message to render.
+	 * @param array $args {
+	 *     Required. The values for the notice.
+	 *
+	 *     @type string $ID       A unique identifier for the notice.
+	 *     @type string $type     The notice's type (one of 'error'|'warning'|'success'|'info').
+	 *     @type mixed  $message  The message to render.
+	 * }
 	 */
-	public function __construct( $type, $message ) {
-		$this->type    = $type;
-		$this->message = $message;
+	public function __construct( $args ) {
+		$this->ID      = $args['ID'];
+		$this->type    = $args['type'];
+		$this->message = $args['message'];
+	}
+
+	/**
+	 * Provide a unique string representation of the notice.
+	 *
+	 * @return string
+	 */
+	public function __toString() {
+		return $this->ID;
 	}
 
 	/**
