2c50eb9b117f7390403338c966cfdcb8a0c2e8fa
Lucas Cherkewski <lucas@ecustom.ca>
2017-08-29 18:40:50 -0400

    correct PHPDoc

--- src/RcpTwilioNotifier/Helpers/Renderers/AdminFormField.php
+++ src/RcpTwilioNotifier/Helpers/Renderers/AdminFormField.php
@@ -19,7 +19,7 @@ class AdminFormField {
 	 * @param string       $id              The field's ID. Used for the field name, too.
 	 * @param string       $label           Short label for the field.
 	 * @param string|array $description     Longer description of the field. If an array, renders a paragraph for each item.
-	 * @param func         $field_callback  A callback to render the field. Receives the field’s ID as a parameter.
+	 * @param callable     $field_callback  A callback to render the field. Receives the field’s ID as a parameter.
 	 * @param array        $args {
 	 *          Optional. Finer control over various settings.
 	 *
