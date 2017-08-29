c8dd01715a9ea7bbf5389c4e26b52de034c2a9ef
Lucas Cherkewski <lucas@ecustom.ca>
2017-08-29 10:26:58 -0400

    placeholder Message model

---
+++ src/RcpTwilioNotifier/Models/Message.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * RcpTwilioNotifier: RcpTwilioNotifier\Models\Message class
+ *
+ * @package WordPress
+ * @subpackage RcpTwilioNotifier\Models
+ */
+
+namespace RcpTwilioNotifier\Models;
+
+/**
+ * A message sent to members.
+ */
+class Message {
+
+}
