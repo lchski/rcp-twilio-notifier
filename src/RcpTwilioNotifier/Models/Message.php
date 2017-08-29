79bbf25ce1b708d0c1bb82a5fc74a93dd7c3bc18
Lucas Cherkewski <lucas@ecustom.ca>
2017-08-29 10:33:32 -0400

    initial properties for the model

--- src/RcpTwilioNotifier/Models/Message.php
+++ src/RcpTwilioNotifier/Models/Message.php
@@ -13,4 +13,32 @@ namespace RcpTwilioNotifier\Models;
  */
 class Message {
 
+	/**
+	 * The recipients of this message.
+	 *
+	 * @var Member[]
+	 */
+	private $recipients;
+
+	/**
+	 * The unprocessed body of the message.
+	 *
+	 * @var string
+	 */
+	private $raw_body;
+
+	/**
+	 * Additional data to include when processing the message.
+	 *
+	 * @var array
+	 */
+	private $body_data;
+
+	/**
+	 * Attempted sends for each recipient, including retries.
+	 *
+	 * @var array
+	 */
+	private $send_attempts;
+
 }
